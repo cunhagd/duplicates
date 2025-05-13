9296b5549e3f0520cf6f90c01c21195bcb1349f5
cunhagdc <gustavocunhaads@gmail.com>
2025-05-13 14:38:36 -0300

    ajuste final de importação dos modulos

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 # Adiciona o diretório 'scr' ao sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'scr')))
 
-from scr.find import main as find_duplicates
-from scr.clean import main as clean_duplicates
+from scr.find import main as find_duplicate_links
+from scr.clean import main as clean_duplicate_links
 
 def main():
     print("Iniciando o processo de verificação e limpeza de duplicatas...")
@@ -13,7 +13,7 @@ def main():
     # Passo 1: Executa o módulo de encontrar duplicatas
     print("\n--- Executando módulo de busca de duplicatas ---")
     try:
-        find_duplicates()
+        find_duplicate_links()
     except Exception as e:
         print(f"Erro ao executar o módulo de busca de duplicatas: {e}")
         sys.exit(1)
@@ -21,7 +21,7 @@ def main():
     # Passo 2: Executa o módulo de limpeza de duplicatas
     print("\n--- Executando módulo de limpeza de duplicatas ---")
     try:
-        clean_duplicates()
+        clean_duplicate_links()
     except Exception as e:
         print(f"Erro ao executar o módulo de limpeza de duplicatas: {e}")
         sys.exit(1)
